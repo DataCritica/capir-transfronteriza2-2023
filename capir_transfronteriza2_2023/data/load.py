353ae852afc138b4e939028769ea964fb5f9d295
Fer Aguirre <kay_op@protonmail.com>
2023-05-13 00:36:36 -0300

    Add path to load assets

--- capir_transfronteriza2_2023/data/load.py
+++ capir_transfronteriza2_2023/data/load.py
@@ -7,10 +7,8 @@ import capir_transfronteriza2_2023.utils.paths as paths
 # Inputs
 data_raw = paths.data_raw_dir()
 data_processed = paths.data_processed_dir()
-data_urls = paths.data_urls_dir()
-data_ids = paths.data_ids_dir()
-data_jsonl = paths.data_jsonl_dir()
 data_minet = paths.data_minet_dir()
+assets = paths.assets_dir()
 
 
 # Outputs
